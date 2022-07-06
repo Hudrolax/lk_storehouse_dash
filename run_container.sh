d64e7bbc7dfa5ee1c7d895a03a0237a550457f7f
NazarovS <s.nazarov@leskraft.ru>
2022-07-06 13:35:11 +0300

    update requirements

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name lk_storehouse_dash --net container:www_nginx --restart unless-stopped -v /app:. lk_storehouse_dash
+docker run -d --name lk_storehouse_dash --net container:www_nginx --restart unless-stopped -v /app:./ lk_storehouse_dash
